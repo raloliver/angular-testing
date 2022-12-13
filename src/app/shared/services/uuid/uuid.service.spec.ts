5afc658a5a601887e3d5a3edfd47bc8df890d4a5
raloliver <contato@raloliver.com.br>
2022-12-13 11:05:42 +0000

    ensure the name of the class and of the method remains

--- src/app/shared/services/uuid/uuid.service.spec.ts
+++ src/app/shared/services/uuid/uuid.service.spec.ts
@@ -1,5 +1,9 @@
 import { UuidService } from './uuid.service';
 
+/**
+ * use .name to get the name of the class
+ * use .prototype.methodName.name to ensure remains the name of the method
+ */
 describe(UuidService.name, () => {
   it(`${UuidService.prototype.generateUuIdWithPrefix.name} should generated generate id when called with prefix`, () => {
     const service = new UuidService();
